883ad4e48a1e4d9ffb9bbef32ad6f63c793ee881
Luis Perez <lpramirez2413@gmail.com>
2016-11-28 15:17:53 -0500

    framework files added

---
+++ OMTools/OMTools.h
@@ -0,0 +1,19 @@
+//
+//  OMTools.h
+//  OMTools
+//
+//  Created by Luis Perez on 28/11/16.
+//  Copyright © 2016 Orbis Ventures. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OMTools.
+FOUNDATION_EXPORT double OMToolsVersionNumber;
+
+//! Project version string for OMTools.
+FOUNDATION_EXPORT const unsigned char OMToolsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OMTools/PublicHeader.h>
+
+
